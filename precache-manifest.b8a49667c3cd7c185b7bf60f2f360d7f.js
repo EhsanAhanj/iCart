aa067ad9b9755377910826f77a92f758a8ef28cc
Ehsan <eng.ahanj@gmail.com>
2020-07-13 15:48:15 +0430

    Updates

--- precache-manifest.b8a49667c3cd7c185b7bf60f2f360d7f.js
+++ precache-manifest.b8a49667c3cd7c185b7bf60f2f360d7f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5928a121be3672944c8181d67bec89f1",
+    "revision": "1977115761cf66a72786c2d987eaa4f1",
     "url": "/iCart/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/iCart/static/css/2.0e35a45d.chunk.css"
   },
   {
-    "revision": "50a3c8be2bb929a7c3b8",
-    "url": "/iCart/static/css/main.34a4778a.chunk.css"
+    "revision": "2f5221558eb410c8248e",
+    "url": "/iCart/static/css/main.afe4b9ec.chunk.css"
   },
   {
     "revision": "56718b8a1fc40f1f315a",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/iCart/static/js/2.369e7c40.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "50a3c8be2bb929a7c3b8",
+    "revision": "2f5221558eb410c8248e",
     "url": "/iCart/static/js/main.d1bfb9d3.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iCart/precache-manifest.ef6ed2881242842b8ed25e045e29f4f3.js"
+  "/iCart/precache-manifest.b8a49667c3cd7c185b7bf60f2f360d7f.js"
 );
 
 self.addEventListener('message', (event) => {
